8e565e2fbb41c346ca316eab8a5e464c4c80db94
Srivastava, Akansha <akansha_srivastava6@optum.com>
2020-06-04 18:44:33 +0530

    Form data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 import {MatGridListModule} from '@angular/material/grid-list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule,MatIconModule} from '@angular/material';
 
+import { FormsModule } from '@angular/forms';
 
+// tslint:disable-next-line: max-line-length
+import {MatNativeDateModule,MatSnackBarModule,MatIconModule,MatDialogModule, MatButtonModule, MatTableModule, MatPaginatorModule , MatSortModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCard, MatCardModule, MatFormField, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule } from '@angular/material';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -26,7 +34,15 @@ import {MatButtonModule,MatIconModule} from '@angular/material';
     MatGridListModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/worker-form/worker-form.component.ts
+++ src/app/worker-form/worker-form.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./worker-form.component.css']
 })
 export class WorkerFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+ // mname: any;
+  lname: any;
+  mob: any;
+  address: any;
+  paddress: any;
+  dob: any;
+  SkillsSet: any;
+  Education: any;
+  Familycount: any;
+ ngOnInit(): void {
+  //  throw new Error("Method not implemented.");
   }
+  
+ isNameSelected: boolean;
+  //fname: any;
+  required: boolean;
+  error: string;
+
+  selectInput(event) {
+    const selected = event.target.value;
+    // tslint:disable-next-line: triple-equals
+    if (selected == 'No') {
+      this.isNameSelected = true;
+    } else {
+      this.isNameSelected = false;
+    }
+  }
+
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+  // tslint:disable-next-line: adjacent-overload-signatures
+  //ngOnInit() {
+  //}
+  
+  constructor() { }
 
 }
